4462525493b912194823a4505cfd1dd4c167aa32
Richard Nemeth <nemethricsi@gmail.com>
2025-05-04 16:26:03 +0200

    fix schema path

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,15 @@
-import { pgTable, serial, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp, pgEnum } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const projectStatus = pgEnum('project_status', ['not_started', 'in_progress', 'completed']);
+
+export const project = pgTable('projects', {
 	id: serial('id').primaryKey(),
-	age: integer('age'),
+	name: text('name').notNull(),
+	description: text('description'),
+	status: projectStatus('status').default('not_started'),
+
+	createdAt: timestamp('created_at', { withTimezone: true }).defaultNow(),
+	updatedAt: timestamp('updated_at', { withTimezone: true })
+		.defaultNow()
+		.$onUpdate(() => new Date()),
 });
