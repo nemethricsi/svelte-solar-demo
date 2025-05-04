e8691e6d6bc15fa7b1712eade3703ccb5eaa536b
Richard Nemeth <nemethricsi@gmail.com>
2025-05-04 17:17:35 +0200

    use env vars

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-// export const supabase = createClient(
-// 	import.meta.env.PUBLIC_SUPABASE_URL,
-// 	import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
-// );
-export const supabase = createClient(
-	'https://trlojuxuggjxwmfezyoe.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRybG9qdXh1Z2dqeHdtZmV6eW9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzNTM3ODEsImV4cCI6MjA2MTkyOTc4MX0.J6cs8pzNwRLjphPqqrJw903_59qufCy3e2wEhd8ldK0',
-);
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
